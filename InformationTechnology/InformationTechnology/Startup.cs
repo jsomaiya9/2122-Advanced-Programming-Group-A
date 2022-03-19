8d7d586af4b4fcb42566419a568b11e80066f488
Michal Mazur <mazurek11331@gmail.com>
2022-03-19 15:51:56 +0000

    CreatedMVCProject /M

---
+++ InformationTechnology/InformationTechnology/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(InformationTechnology.Startup))]
+namespace InformationTechnology
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
