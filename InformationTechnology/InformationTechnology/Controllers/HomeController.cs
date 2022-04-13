4966794ec3ff0657922aac49dd46e6d42f8e87f0
Michal Mazur <mazurek11331@gmail.com>
2022-04-13 14:51:54 +0100

    ...

--- InformationTechnology/InformationTechnology/Controllers/HomeController.cs
+++ InformationTechnology/InformationTechnology/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace InformationTechnology.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Need help? Contact Us.";
 
             return View();
         }
